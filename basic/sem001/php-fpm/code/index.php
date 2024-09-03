595443b1ca11b557f62c666f433a515c678967c0
Nikita Matveev <nikitamatveev92@gmail.com>
2024-09-03 21:04:57 +1000

    php hw001 done

---
+++ basic/sem001/php-fpm/code/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$name = 'Admin';
+
+echo $name;
+var_dump($name);
+
+// echo "Привет, GeekBrains!<br>".date("Y-m-d H:i:s") ."<br><br>";
+
+// echo "Что-то еще 123456";
+
+// phpinfo(); // для просмотра доступных в моей сборке модулей
