9de1e84997a0757eb3eed10212b04778a7f1e8f1
Nikita Matveev <nikitamatveev92@gmail.com>
2024-12-16 00:35:36 +1000

    PHP hw008 done

---
+++ basic/hw008/code/src/Application/Application.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Geekbrains\Application1\Application;
+
+use Geekbrains\Application1\Domain\Controllers\AbstractController;
+use Geekbrains\Application1\Infrastructure\Config;
+use Geekbrains\Application1\Infrastructure\Storage;
+use Geekbrains\Application1\Application\Auth;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Level;
+use Monolog\Handler\FirePHPHandler;
+
+class Application
+{
+
+    private const APP_NAMESPACE = 'Geekbrains\Application1\Domain\Controllers\\';
+
+    private string $controllerName;
+    private string $methodName;
+
+    public static Config $config;
+
+    public static Storage $storage;
+
+    public static Auth $auth;
+
+    //созадем переменную логов
+    public static Logger $logger;
+
+    public function __construct()
+    {
+        Application::$config = new Config();
+        Application::$storage = new Storage();
+        Application::$auth = new Auth();
+
+        //создаем для логирования
+        Application::$logger = new Logger('application_logger');
+        Application::$logger->pushHandler(
+            new StreamHandler(
+                $_SERVER['DOCUMENT_ROOT'] . "/log/" . Application::$config->get()['log']['LOGS_FILE'] . "-" . date("Y-m-d") . ".log",
+                Level::Debug
+            )
+        );
+
+        // для совместимости
+        Application::$logger->pushHandler(new FirePHPHandler());
+    }
+
+    public function run(): string
+    {
+        session_start();
+        Auth::RestoreSession();
+
+        $routeArray = explode('/', $_SERVER['REQUEST_URI']);
+
+        if (isset($routeArray[1]) && $routeArray[1] != '') {
+            $controllerName = $routeArray[1];
+        } else {
+            $controllerName = "page";
+        }
+
+        $this->controllerName = Application::APP_NAMESPACE . ucfirst($controllerName) . "Controller";
+
+        if (class_exists($this->controllerName)) {
+            if (isset($routeArray[2]) && $routeArray[2] != '') {
+                $methodName = $routeArray[2];
+            } else {
+                $methodName = "index";
+            }
+
+            $this->methodName = "action" . ucfirst($methodName);
+
+            if (method_exists($this->controllerName, $this->methodName)) {
+                $controllerInstance = new $this->controllerName();
+
+                if ($controllerInstance instanceof AbstractController) {
+                    if ($this->checkAccessToMethod($controllerInstance, $this->methodName)) {
+                        return call_user_func_array(
+                            [$controllerInstance, $this->methodName],
+                            []
+                        );
+                    } else {
+                        return "Нет доступа к методу";
+                    }
+                } else {
+                    return call_user_func_array(
+                        [$controllerInstance, $this->methodName],
+                        []
+                    );
+                }
+            } else {
+                $logMessage = "Метод " . $this->methodName . " не существует в контроллере " . $this->controllerName . " | ";
+                $logMessage .= "Попытка вызова адреса " . $_SERVER['REQUEST_URI'];
+                Application::$logger->error($logMessage);
+
+                return "Метод не существует";
+            }
+        } else {
+            return "Класс $this->controllerName не существует";
+        }
+    }
+
+    private function checkAccessToMethod(AbstractController $controllerInstance, string $methodName): bool
+    {
+        $userRoles = $controllerInstance->getUserRoles();
+
+        $rules = $controllerInstance->getActionsPermissions($methodName);
+
+        $rules[] = 'user';
+
+        $isAllowed = false;
+
+        if (!empty($rules)) {
+            foreach ($rules as $rolePermission) {
+                if (in_array($rolePermission, $userRoles)) {
+                    $isAllowed = true;
+                    break;
+                }
+            }
+        }
+
+        return $isAllowed;
+    }
+}
