6ce94ebee4b093a9c13efe7628a92843e3682db0
Nikita Matveev <nikitamatveev92@gmail.com>
2024-12-17 00:43:25 +1000

    php hw009 done

---
+++ basic/hw009/code/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$memory_start = memory_get_usage();
+
+
+require_once('../vendor/autoload.php');
+
+use Geekbrains\Application1\Application\Application;
+
+try{
+    $app = new Application();
+    echo $app->run();
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+
+$memory_end = memory_get_usage();
+
+echo "<h4>Потреблено " . ($memory_end - $memory_start)/1024/1024 . " Мбайт памяти</h4>";
